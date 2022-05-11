608159e8cc3e2128718787192820e9e023889afb
Sanmeet09 <sanmeet.planetodoo@gmail.com>
2022-05-11 15:40:13 +0530

    One2many field added in customerdetail.py

--- NewSales/models/customerdetails.py
+++ NewSales/models/customerdetails.py
@@ -9,3 +9,16 @@ class CustomerDetails(models.Model):
     age = fields.Integer('Age')
     mobile = fields.Char('Mobile No')
     gender = fields.Selection(selection=[('m', 'Male'), ('f', 'Female')], default='m', String='Gender')
+
+    product_ids = fields.One2many('customerdetail.line', 'product_id', 'Product line')
+
+
+class CustomerDetailLine(models.Model):
+    _name = 'customerdetail.line'
+    _description = 'one2many field in customer'
+
+    product = fields.Char('Product')
+    quantity = fields.Integer('Quantity')
+    price = fields.Integer('Price')
+
+    product_id = fields.Many2one('customer.detail','Product Id')
